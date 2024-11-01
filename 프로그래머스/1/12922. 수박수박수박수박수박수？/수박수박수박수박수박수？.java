6284782c4497dc9b14ce54e1ff856806ee032abf
Ddu_lut99 <su9926@naver.com>
2024-11-01 23:37:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.65 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb=new StringBuilder("");
+        for(int i=1; i<=n; i++){
+            if(i%2!=0){
+                sb.append("수");
+            }else{
+                sb.append("박");
+            }
+        }
+        return sb.toString();
+    }
+}
