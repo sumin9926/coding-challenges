5eb3437e7a4895dbddaf2643ea8042381c3a59e6
Ddu_lut99 <su9926@naver.com>
2024-11-08 11:58:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.07 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -6,22 +6,15 @@ class Solution {
         int maxNum=Math.max(n,m); //n,m 중 더 큰 수
         
         //최대공약수 구하기
-        for(int i=1; i<=minNum; i++){
-            if(n%i==0 && m%i==0){
-                answer[0]=i;
-            }
-        }
-        
-        int cnt=2;
-        int maxNumTimes=maxNum;
-        //최소공배수 구하기
         while(true){
-            if(maxNumTimes%minNum==0){
-                answer[1]=maxNumTimes;
+            if(minNum==0){
+                answer[0]=maxNum;
+                answer[1]=n*m/maxNum;
                 break;
-            }else{
-             maxNumTimes=maxNum*cnt++;  
             }
+            int tmp=maxNum%minNum;
+            maxNum=minNum;
+            minNum=tmp;
         }
         
         return answer;
