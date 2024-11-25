92043f561d8c7e754d33479caab08373207dd1ae
Ddu_lut99 <su9926@naver.com>
2024-11-25 11:46:06 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 6.94 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] numbers) {
+    public TreeSet<Integer> solution(int[] numbers) {
         TreeSet<Integer> treeSet=new TreeSet<Integer>((o1,o2)->o1-o2); //index가 다른 두 수를 더한 값을 저장하는 set
         for(int i=0; i<numbers.length-1;i++){
             for(int j=i+1; j< numbers.length; j++){
                 treeSet.add(numbers[i]+numbers[j]);
             }
         }
-        int[] answer=treeSet.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+       
+        return treeSet;
     }
 }
