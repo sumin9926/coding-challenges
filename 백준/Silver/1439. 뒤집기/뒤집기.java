6a83965b000745fe50a349a5b390522f147d97df
Ddu_lut99 <su9926@naver.com>
2025-07-09 16:04:30 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -11,21 +11,24 @@ public class Main {
     }
 
     public static int cntStringReversal(String input) {
+        if(input.length()==1) return 0;
         int cntOne = 0, cntZero = 0;
-        char[] inputArr = input.toCharArray();
-        for (int i = 1; i < inputArr.length; i++) {
-            if (inputArr[i - 1] != inputArr[i]) {
-                if (inputArr[i - 1] == '0') {
-                    cntZero++;
-                } else {
-                    cntOne++;
-                }
-            }
-        }
-        if(inputArr[inputArr.length-1]=='0'){
+        char prevChar = input.charAt(0);
+        if(prevChar=='0'){
             cntZero++;
         }else{
             cntOne++;
+        }
+        for (int i = 1, n = input.length(); i < n; i++) {
+            char currentChar = input.charAt(i);
+            if(prevChar!=currentChar){
+                if(currentChar=='0'){
+                    cntZero++;
+                }else{
+                    cntOne++;
+                }
+                prevChar=currentChar;
+            }
         }
 
         return Math.min(cntOne, cntZero);
