205d5c56c90bed28cd85e5829b844dc05126dadd
Ddu_lut99 <su9926@naver.com>
2025-07-15 00:11:03 +0900

    [Gold V] Title: 회문, Time: 356 ms, Memory: 37388 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String[] strArr = new String[num];
+        for (int i = 0; i < num; i++) {
+            strArr[i] = br.readLine();
+        }
+        for (String str : strArr) {
+            System.out.println(isPalindrome(str));
+        }
+    }
+
+    public static int isPalindrome(String str) {
+        char[] arr = str.toCharArray();
+        for (int i = 0, j = str.length() - 1; i <= j; i++, j--) {
+            if (arr[i] != arr[j]) {
+                if(checkPalindrome(arr, i, j-1) || checkPalindrome(arr, i+1, j)) return 1;
+                else return 2;
+            }
+        }
+        return 0;
+    }
+
+    public static boolean checkPalindrome(char[] charArr, int i, int j){
+        if(i>=j) {
+            return true;
+        }
+        if(charArr[i]==charArr[j]){
+           return checkPalindrome(charArr, i+1, j-1);
+        }else return false;
+    }
+}
