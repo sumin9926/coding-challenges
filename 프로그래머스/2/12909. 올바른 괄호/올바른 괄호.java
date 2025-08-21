d70078f353916bdeb0e6eb6a8b73f22a05d8f67a
Ddu_lut99 <su9926@naver.com>
2025-08-21 23:24:56 +0900

    [level 2] Title: 올바른 괄호, Time: 16.33 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    boolean solution(String s) {
+        Deque<Character> dq = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if(c=='('){
+                dq.addFirst(c);
+            }else{
+                if(dq.isEmpty()) return false;
+                dq.pop();
+            }
+        }
+
+        return dq.isEmpty();
+    }
+}
