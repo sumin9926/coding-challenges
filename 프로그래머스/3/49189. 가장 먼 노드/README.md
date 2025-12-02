a5e17345782c49e6168ee2936cf1ef4f2dbd6a47
Ddu_lut99 <su9926@naver.com>
2025-12-02 20:25:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 54.51 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=javascript) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 4017.36 ms
+메모리: 56.1 MB, 시간: 54.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 02일 18:17:30
+2025년 12월 02일 20:25:46
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,33 @@
+function solution(n, edge) {
+    let answer = 0, max = 0;
+    let graph = Array.from(Array(n+1), ()=>[]);
+    let nodes = new Array(n+1).fill(-1);
+    let pq = [];
+    
+    // 그래프 정보 채우기
+    for(let row of edge){
+        let v1 = row[0], v2=row[1];
+        graph[v1].push(v2);
+        graph[v2].push(v1);
+    }
+    
+    // 1번 노드 - 각 노드 사이의 간선 개수 구하기
+    pq.push(1);
+    nodes[1]=0;
+    while(pq.length>0){
+        let node = pq.shift();
+        for(let v of graph[node]){
+            if(nodes[v]===-1){
+                nodes[v] = nodes[node]+1;
+                if(max<nodes[v]){
+                    max = nodes[v];
+                    answer=0;
+                }
+                answer++;
+                pq.push(v);
+            }
+        }
+    }
+    
+    return answer;
+}
