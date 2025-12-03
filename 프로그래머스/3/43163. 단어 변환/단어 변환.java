539371701a7d283e312e77b64233bb545e742589
Ddu_lut99 <su9926@naver.com>
2025-12-03 17:38:01 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,61 +1,46 @@
 import java.util.*;
 
 class Solution {
+    static class Word{
+        String str;
+        int step;
+        
+        Word(String str, int step){
+            this.str=str;
+            this.step=step;
+        }
+    }
+    
     public boolean matchWords(String str1, String str2){
-        int unMatchCnt = 0;
-        int[] cntChar = new int[98];
-        
+        int unMatch=0;
         for(int i=0; i<str1.length(); i++){
-            cntChar[str1.charAt(i)-97]++;
+            if(str1.charAt(i)!=str2.charAt(i)){
+                unMatch++;
+                if(unMatch>=2) return false;
+            }
         }
-        
-        for(int i=0; i<str2.length(); i++){
-            cntChar[str2.charAt(i)-97]--;
-        }
-        
-        for(int v : cntChar){
-            unMatchCnt+=Math.abs(v);
-            if(unMatchCnt>2) return false;
-        }
-        
         return true;
     }
     
     public int solution(String begin, String target, String[] words) {
-        Map<String, ArrayList<String>> wordsGraph = new HashMap<>();
-        Map<String, Integer> cntSteps = new HashMap<>(); //문자열별 최소 변환 단계 저장
-        for(String str:words){
-            cntSteps.put(str, -1);
-        }
-        cntSteps.put(begin, 0);
+        int totalWords = words.length;
+        boolean[] isVisited = new boolean[totalWords];
         
-        // 상호 변환 가능 문자열 그래프 만들기
-        for(int i=0; i<=words.length; i++){
-            String str1 = null;
-            if(i==words.length) str1 = begin;
-            else str1 = words[i];
-            for(int j=0; j<words.length; j++){
-                if(j==i) continue;
-                if(matchWords(str1, words[j])){
-                    wordsGraph.computeIfAbsent(str1, k -> new ArrayList<>()).add(words[j]);
-                }
-            }
-        }
-        
-        // BFS
-        Deque<String> dq = new ArrayDeque<>();
-        dq.offerLast(begin);
+        Deque<Word> dq = new ArrayDeque<>();
+        dq.offerLast(new Word(begin, 0));
         
         while(!dq.isEmpty()){
-            String currentString = dq.pollFirst();
-            for(String nextString : wordsGraph.get(currentString)){
-                if(nextString.equals(target)){
-                    return 1+cntSteps.get(currentString);
+            Word currentWord = dq.pollFirst();
+            if(currentWord.str.equals(target)){
+                return currentWord.step;
+            }
+            for(int i=0; i<totalWords; i++){
+                if(isVisited[i]) continue;
+                String nextWord = words[i];
+                if(matchWords(currentWord.str, nextWord)){
+                    isVisited[i]=true;
+                    dq.offerLast(new Word(nextWord, currentWord.step+1));
                 }
-                Integer cnt = cntSteps.get(nextString);
-                if(cnt!=-1) continue;
-                cntSteps.put(nextString, 1+cntSteps.get(currentString));
-                dq.offerLast(nextString);
             }
         }
         
