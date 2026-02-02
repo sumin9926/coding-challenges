6e9392befd6fbc09d07b21c7d2e6d94f06a4464c
Ddu_lut99 <su9926@naver.com>
2026-02-02 16:17:55 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        if (nums.length == 1) System.out.print(nums[0] * nums[0]);
+        else {
+            Arrays.sort(nums);
+            System.out.print(nums[0] * nums[N - 1]);
+        }
+    }
+}
