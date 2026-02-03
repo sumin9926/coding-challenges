1b6654edb219f3212964ecee1f630fef4f333f2d
Ddu_lut99 <su9926@naver.com>
2026-02-03 17:22:05 +0900

    [Silver III] Title: 칸토어 집합, Time: 272 ms, Memory: 27372 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Main {
+    static char[] chars;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        while (!Objects.equals(line = br.readLine(), null)) {
+            int N = Integer.parseInt(line);
+            int strLength = pow(3, N);
+            chars = new char[strLength];
+            Arrays.fill(chars, '-');
+
+            // 칸토어 집합 근사 만들기
+            cantor(0, strLength - 1);
+            // 답 저장
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < strLength; i++) {
+                result.append(chars[i]);
+            }
+            sb.append(result).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void cantor(int start, int end) {
+        if (start < end) {
+            int changeNum = (end - start + 1) / 3;
+            int midStart = start + changeNum, midEnd = end - changeNum;
+            for (int i = midStart; i <= midEnd; i++) {
+                chars[i] = ' ';
+            }
+            cantor(start, midStart - 1);
+            cantor(midEnd + 1, end);
+        }
+    }
+
+    static int pow(int a, int n) {
+        int result = 1;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                result *= a;
+            }
+            a *= a;
+            n >>= 1;
+        }
+        return result;
+    }
+}
