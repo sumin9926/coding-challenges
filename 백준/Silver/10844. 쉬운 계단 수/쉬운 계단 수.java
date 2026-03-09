3d8ff2ef4e472a52087f6f0b906f0d7a9b92f732
Ddu_lut99 <su9926@naver.com>
2026-03-09 17:59:58 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][1];
+            dp[i][9] = dp[i - 1][8];
+            for (int j = 1; j <= 8; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+            }
+        }
+
+        long answer = 0;
+        for (int i = 0; i <= 9; i++) {
+            answer = (dp[N][i] + answer) % MOD;
+        }
+        System.out.print(answer);
+    }
+}
