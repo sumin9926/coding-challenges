681d29846807210f9587e950f808d9dc0ed11421
Ddu_lut99 <su9926@naver.com>
2026-03-14 15:06:43 +0900

    [Gold IV] Title: LCS, Time: 132 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine(), str2 = br.readLine();
+        int len1 = str1.length(), len2 = str2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            char c1 = str1.charAt(i - 1);
+            for (int j = 1; j <= len2; j++) {
+                if (c1 == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        System.out.print(dp[len1][len2]);
+    }
+}
