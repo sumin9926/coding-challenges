3d817efc6cb3f12b86e25f439ba36dcfa8710c02
Ddu_lut99 <su9926@naver.com>
2026-03-17 16:26:23 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 504 ms, Memory: 53712 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,67 +4,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static int i, j, result;
+    static int[] preSums;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        preSums = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        preSums[0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N; i++) {
+            preSums[i] = preSums[i - 1] + Integer.parseInt(st.nextToken());
         }
 
         StringBuilder sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
-        i = Integer.parseInt(st.nextToken()) - 1;
-        j = Integer.parseInt(st.nextToken()) - 1;
-        prefixSum(i, j);
-        sb.append(result).append('\n');
-
-        for (int k = 1; k < M; k++) {
+        for (int k = 0; k < M; k++) {
             st = new StringTokenizer(br.readLine());
-            int ni = Integer.parseInt(st.nextToken()) - 1;
-            int nj = Integer.parseInt(st.nextToken()) - 1;
-            prefixSum(ni, nj, result);
-            sb.append(result).append('\n');
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            if (i > 0) sb.append(preSums[j] - preSums[i - 1]).append('\n');
+            else sb.append(preSums[j]).append('\n');
         }
         System.out.print(sb);
-    }
-
-    private static void prefixSum(int i, int j) {
-        for (int idx = i; idx <= j; idx++) {
-            result += arr[idx];
-        }
-    }
-
-    private static void prefixSum(int ni, int nj, int preResult) {
-        int nResult = preResult;
-        if (ni > i) {
-            for (int idx = i; idx < ni; idx++) {
-                nResult -= arr[idx];
-            }
-        } else if (ni < i) {
-            for (int idx = ni; idx < i; idx++) {
-                nResult += arr[idx];
-            }
-        }
-
-        if (nj > j) {
-            for (int idx = j + 1; idx <= nj; idx++) {
-                nResult += arr[idx];
-            }
-        } else if (nj < j) {
-            for (int idx = j; idx > nj; idx--) {
-                nResult -= arr[idx];
-            }
-        }
-
-        i = ni;
-        j = nj;
-        result = nResult;
     }
 }
